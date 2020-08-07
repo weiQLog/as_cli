794b58a6c5d9b1e61f488bfddf94f655e8c2d899
Rem <1505774577@qq.com>
2020-08-07 16:29:51 +0800

    补充更新

--- dist/static/js/2.f099e08274408147851c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{VXsF:function(n,w,o){}}]);
-//# sourceMappingURL=2.f099e08274408147851c.js.map
--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 import { hello } from "./assets/js/promise";
-import ("./assets/css/app.css")
+import("./assets/css/app.css");
+import ElementUI from "element-ui";
 import App from "./App.vue";
 import Vue from "vue";
-import router from './router'
+import router from "./router";
 
 hello().then((res) => {
   console.log(res);
 });
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+Vue.use(ElementUI);
+Vue.use(router);
+const root = document.createElement("div");
+document.body.appendChild(root);
 
 new Vue({
+  // 注入到根实例中
   render: (h) => h(App),
-  router
-}).$mount(root)
+  router,
+}).$mount(root);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
