d75304e9361af7c11bd82841a5ea06d1452724ac
weiQi <1505774577@qq.com>
2021-02-22 10:33:29 +0800

    添加前置守卫实现可以在路由中指定页面document.title

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import login from 'components/Login/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '',
@@ -18,3 +18,15 @@ export default new Router({
     },
   ],
 })
+
+router.beforeEach((to, from, next) => {
+  //beforeEach是router的钩子函数，在进入路由前执行
+  if (to.meta.title) {
+    //判断是否有标题
+    document.title = to.meta.title
+  }
+  console.log('前置守卫', to)
+  next()
+})
+
+export default router
